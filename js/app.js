a2d658d4543dcb1d6de46fc15e87ae3eeb34dbd4
simeon <simeon1929@gmail.com>
2021-10-19 23:36:06 +0530

    used operator

--- js/app.js
+++ js/app.js
@@ -16,3 +16,52 @@ y = 30
 var resultDec = y--
 
 console.log(y);
+
+
+//Bollen
+
+let a = 10;
+let b = 20;
+
+
+console.log(a < b);  //true
+console.log(5 < 10);
+
+console.log(5==5);
+
+console.log('5' ==5);
+
+console.log(5 !=10);
+
+
+console.log('false value');
+//false value
+
+console.log(4 == 10);
+console.log(4 >10);
+console.log(4===10);
+
+
+console.log('OR operator ||');
+console.log(10 > 5 || 10 < 5);
+
+console.log(5< 3 || 3 < 1 );
+
+
+console.log('and operator');
+
+console.log(5<8 && 4<7);
+
+
+
+var name = 'simeon'
+var age = 20
+
+console.log(typeof(name));
+console.log(typeof(age));
+
+console.log('my name is', name , 'and my age is', age);
+
+//false 
+
+
