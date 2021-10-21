fe521a714e14b3cc2164cf5cfe8800e3e9c28638
simeon <simeon1929@gmail.com>
2021-10-22 00:14:31 +0530

    form refresh and empty data error

--- js/login.js
+++ js/login.js
@@ -2,16 +2,26 @@ const loginForm = document.querySelector('.login')
 const username = document.getElementById('userName')
 const password = document.getElementById('Password')
 const submit = document.getElementById('submit')
+const loginerror = document.querySelector('.error__bar')
+const loginerror2 = document.querySelector('.error__bar2')
 
 
 loginForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     
-
-    if(username.value == password.value) {
+    if (username.value == " " == password ==" ") { 
+        loginerror2.classList.add('active')
+        alert('empty') 
+    }
+    else if(username.value == password.value) {
         console.log('correct'); 
         window.location = 'welcome.html'; }
-    else 
-        console.log('please enter correct name');  
+    else  {
+        loginerror.classList.add('active') 
+        alert('WRONG DATA')
+    }
+
+    
+    loginForm.reset()       
     
 })
